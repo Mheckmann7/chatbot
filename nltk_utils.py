c768abe5e069939d523e2b548fd7d62e700ddd36
Mira Heckmann <miraheckmann@gmail.com>
2021-03-29 18:39:35 -0700

    adds nltk

---
+++ nltk_utils.py
@@ -0,0 +1,23 @@
+
+from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('punkt')
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower)
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+
+a = "How long does shipping take?"
+print(a)
+a = tokenize(a)
+print(a)
