6c7b75d2d1a628438c613404c79902541c10747b
Mira Heckmann <miraheckmann@gmail.com>
2021-03-29 19:17:35 -0700

    adds json and train

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,7 +1,8 @@
 
 from nltk.stem.porter import PorterStemmer
 import nltk
-nltk.download('punkt')
+import numpy as np
+# nltk.download('punkt')
 stemmer = PorterStemmer()
 
 
@@ -10,14 +11,13 @@ def tokenize(sentence):
 
 
 def stem(word):
-    return stemmer.stem(word.lower)
+    return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
-
-
-a = "How long does shipping take?"
-print(a)
-a = tokenize(a)
-print(a)
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w, in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
